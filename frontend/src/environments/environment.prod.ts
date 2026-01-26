c300ce279fe7206cec4790f20594eef8ecf39adb
Jayedinai <jaydobariyaedinai@gmail.com>
2026-01-26 17:54:19 +0530

    Change

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://college-final-project.onrender.com/api', // Replace with YOUR Render URL
-    socketUrl: 'https://college-final-project.onrender.com'   // Replace with YOUR Render URL
+    apiUrl: '/api',
+    socketUrl: ''
 };

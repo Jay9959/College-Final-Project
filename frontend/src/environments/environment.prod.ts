3802af1bdf05c8529147dc54ecd64004c1447606
Jay9959 <dobariyajay9959@gmail.com>
2026-02-04 17:50:39 +0530

    change the file environments file

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,9 +1,5 @@
 export const environment = {
   production: true,
-<<<<<<< HEAD
-  apiUrl: 'https://college-final-project.onrender.com/api'
-=======
   apiUrl: 'https://college-final-project.onrender.com/api',
   socketUrl: 'https://college-final-project.onrender.com'
->>>>>>> origin/main
 };
